abaac4398f2c33c238b8088c6c5cd36b1b5b0a51
Nicolas Pavon <nicosubealanube@gmail.com>
2022-06-23 02:15:30 -0300

    post controller

--- api/src/controllers/videogames.js
+++ api/src/controllers/videogames.js
@@ -22,16 +22,79 @@ try {
         res.status(200).send(videogames) 
     }
     console.log(4)
-}
+    }
 catch(error) {
     res.status(400).send({errorMsg: error})
-}
+    }
 };
 
 // ruta por id --------
+
+const getGameById = async (req, res)=> {
+try{
+    const{id} = req.params
+    if(id.length > 7 && typeof id === 'string'){
+        const dbGame = await getDbInfo();
+        const gameId = await dbGame.filter((e)=>e.id === id);
+        return res.status(200).send(gameId)
+    } else{ /* que busque en api*/
+        const apiGame = await getOneGame(id);
+        return res.status(200).send(apiGame)
+    }
+    }
+catch(error){
+    res.status(404).send({errorMsg:"videogame not found" })
+    }
+}
+
+// ruta del post --------
+
+const postVideogame = async(req, res)=>{
+try{
+    const {name,
+        background_image,
+        description,
+        released,
+        rating,
+        genres,
+        platforms,
+        createdVideoGame,} = req.body;
+    if(!name || !description || !genres) {
+        res.status(400).send('Missing data')
+    }
+    let newGame = await Videogame?.create({
+        name,
+        background_image:
+        background_image ||
+        "https://i.pinimg.com/originals/ba/e1/de/bae1de77d6e67ae8bb6250941c4256c1.jpg",
+        description,
+        released,
+        rating,
+        platforms,
+        createdVideoGame,
+    })
+    genres.forEach(async (e) => {
+        let genreDb = await Genres.findAll({
+            where: {
+                name: e
+            }
+        })
+        await newGame.addGenres(genreDb);
+    })
+    res.status(200).json(newGame);
+    }
+catch(error) {
+    res.status(400).send({errorMsg: error});
+    }
+}
+
+// ruta del put ------
+
 
 
 
 module.exports= {
     getVideogame,
+    getGameById,
+    postVideogame,
 };
--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const {getVideogame} = require('../controllers/videogames');
+const {getVideogame, getGameById, postVideogame} = require('../controllers/videogames');
 
 const videogamesRouter = express.Router();
 
 videogamesRouter.get('/', getVideogame);
+videogamesRouter.get('/:id', getGameById);
+videogamesRouter.post('/', postVideogame);
 
 
 
