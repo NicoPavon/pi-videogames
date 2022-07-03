d71ec38d3ec7bcd5b71a4c524f4046bfa08c883e
Nicolas Pavon <nicosubealanube@gmail.com>
2022-07-03 19:43:44 -0300

    error404 created

--- client/src/components/error404/error404.jsx
+++ client/src/components/error404/error404.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './error404.css';
 
 const Page404 = () =>{
     return(
         <div className='container-404'>
-        <div><p className='write-404'>404 error! Press Home to restart</p></div>
+        <div><p className='write-404'>, PRESS THE HOME BUTTON!</p></div>
         <div className='container-btn-404'>
            <Link to='/Home'>
-            <button className='btn-404'>
-                <strong>HOME</strong>
+            <button className='btn-class'>
+                <strong>Home</strong>
             </button>
             </Link>
         </div>
